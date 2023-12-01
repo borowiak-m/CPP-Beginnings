718589fe89d15bdb5f7c93f81a8dd6f80774e41f
borowiak-m <neutron.meme@proton.me>
2023-12-01 20:04:34 +0000

    complete logic explanation

--- Backgammon/main.cpp
+++ Backgammon/main.cpp
@@ -1,64 +1,87 @@
+/* BACKGAMMON GAME IN TERMINAL
 
-/* 
-rzeczowniki:
-    diceD6
-    diceDoubling
-    player
-    gameBoard
-    boardPositions
-    middleBar
-    whitesHome
-    whitesBase
-    redsHome
-    redsBase
-    redCheckers
-    whiteCheckers            
-    gameStartIndicator       - who starts first
-    blockingNumberOfCheckers - number of checkers in position blocking a move onto the position
-    moveDirectionIdicator    - direction ( 24->1 "-" / 1->24 "+" )
-    winPoints                - punktacja
-    winCount                 - liczba zwyciestw
+UNDERSTANDING THE GAME DATA DICTIONARY
+NOUNS:
+diceD6
+diceDoubling
+player
+gameBoard
+boardPositions
+middleBar
+whitesHome
+whitesBase
+redsHome
+redsBase
+redCheckers
+whiteCheckers            
+gameStartIndicator       - who starts first
+blockingNumberOfCheckers - number of checkers in position blocking a move onto the position
+moveDirectionIdicator    - direction ( 24->1 "-" / 1->24 "+" ) reds move one direction, whites move the other direction
+winPoints                
+winCount                 
 
-czasowniki:
-    redOrWhiteChoice         - wymor kolou pionkow ( = pozycji na planszy, bazy i domu oraz kierunku ruchow)
-    diceRollStart            - poczatkowy rzut kosci
-    startDiceRollCompare     - porownanie wynikow kosci
-    diceRollInGame           - rzuc koscmi
-    boardInitialSetup        - pierwsze ustawienie planszy
-    moveChecker              - ruch pionka z pozycji startowej na pozycje plus oczka kosci
-    hitChecker               - zbicie pionka z pozycji (tylko kiedy = 1 pionek przeciwnika na pozycji)
-    moveCheckerToBase        - zdjecie pionka z planszy do bazy
-    moveLegalityCheck        - czy mozna sie ruszyc z poz startowej plus oczka z kosci (jesli >=2 pionki przeciwnika to nie) 
-    allBackHomeCheck         - czy wszystkie pionki sa w domu
-    middleBarCheck           - czy jest moj pionek na srodku planszy
-    winConditionCheck        - czy wszystkie pionki sa w bazie
-    calculateWinningPoints   - kalulacja punktacji (w zaleznosci od ukladu planszy)
-    moveToBasePriorityCheck  - spr ktory pionek moze wejsc do bazy pierwszy (ten najdalej)
-
-
-    [ZAPISYWANE W TRAKCIE GRY:
-        - Stan serii, czy kontynuacje czy nowa seria
-        - Stan rozgrywki:   kolor pionkow, pozycje wszystkich pionkow, kogo ruch, 
-                            rzut kosci dla ruchu (domyslnie 0, 0, po rzucie update na X, X z wyniku rzutu)
-    ]*/
+VERBS/ACTIONS:
+redOrWhiteChoice         - player either plays reds or whites
+diceRollStart            - there is a starting dice roll to determine order of play, who goes first
+startDiceRollCompare     
+diceRollInGame           
+boardInitialSetup        - there is a set way how the board is setup at the geginning of the game
+moveChecker              - checkers can move to a position on the board = starting position + roll dice number / or - roll dice depends on the movement direction
+hitChecker               - hit opponents checker off a position onto the middle of the board / only when 1 oppositing checker, if more the move is illegal
+moveCheckerToBase        - taking a checker off board into base, when all checkers in base = win game
+moveLegalityCheck        - if there is 2 or more opponent's checkers then a board position is considered off limits = illegal
+allBackHomeCheck         - check if all checkers are home = each player has defined home range of positions
+middleBarCheck           - check if player has checkers in the middle of the board
+winConditionCheck        - when all checkers are off board, in base
+calculateWinningPoints   - points calculated when game ends, depending on the position of opponents 
+moveToBasePriorityCheck  - checking which checkers have priority to move to the base from the board
 
 
-    // 1. Rozpoczecie gry       (pojedyncza czy seria do okreslonej liczby wygranych ??)
+[SAVING GAME STATUS:
+When:
+Game status is saved every round when board is updated.
+How:
+Game status is saved to a text file in a format variable1|variable2|variable3...etc, each having a precise position in the data format
+- Game status params:   gameSerieNumber,      - current serie
+                        gameMatchNumber,      - current match in a serie
+                        gameRoundNumber,      - current round in a match
+                        player1wins,          - player1 won matches in current game serie
+                        player1color, 
+                        player1name, 
+                        player1diceroll,      - player1 dice roll values if they rolled, if not = 0 0 0 0
+                        player2wins,          - player2 won matches in current game serie
+                        player2color, 
+                        player2name,          
+                        player2diceroll,      - player2 dice roll values if they rolled, if not = 0 0 0 0
+                        boardState            - number of each player's checkers per board position
+]*/
 
-      //  - sprawdzenie numeru serii/rozgrywki 
-      //  - wybor koloru pionkow przez gracza
-      //  - ustawienie planszy (umieszczenie pionkow na ustalonych pozycjach)
-      //  - poczatkowy rzut koscia, 2x D6 dla gracza, 2x D6 dla komputera
-      //  - porownanie wynikow i ustalenie pierszenstwa gry
-
-    // 2. Gra
-
-       // - pierwszy gracz rzuca kosia 2x D6 dla ruchow
-       // - wedluch rezultatow rzutu koscmi wygeneruj liste legalnych ruchow
-       // - jesli graczem jest komputer, zagraj pierwszy ruch z listy
-       // - jesli graczem jest czlowiek, podaj liste legalnych ruchow do wyboru
 
 
-       int main() {
-        
-       }
+int main() {
+
+// 1. Game start      
+
+//    Determine if it's a new game or a continuation 
+//      If it's a new game, initiate default game params and actions
+//        Player color choice
+//        Initiative dice roll who starts first
+//        Default board setup
+//      If it's a continuation of a game
+//        Read game details from save file and assign params
+//        Continue the game by going to the next player round
+
+// 2. Game logic
+
+//    Game is in progress as long as both players do not have all their checkers in thir base. Once one of them does, it is the win condition and game ends.
+//      When game ends, win count and winning points are updated, game final save is done, and moved from current game save to game history file
+
+//    Each round the following actions happen: 
+//      Player rolls 2 d6 dice for movement
+//      Based on dice results all legal movements are determined (if any checkers are in the middle of the board, they need to be moved as a priority)
+//      Player makes choice where to move checkers
+//      Board gets updated on screen, and game is saved to file 
+//    
+
+  return 0;
+}
